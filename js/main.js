a463c3534c410610e583c424474192c89cfffa53
JEHR <jehr@dr.dk>
2015-03-05 15:43:37 +0100

    Add auto-redirect JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+(function() {
+	var timer = 1000,
+		times = 10,
+		el = document.querySelector(".secs");
+	el.textContent = times;
+	setInterval(function() {
+		if (times === 0) {
+			window.location = "http://drdk.github.io/docs-ux/layouts/";
+		} else {
+			times--;
+			el.textContent = times;
+		} 
+	}, timer);
+})(window);
